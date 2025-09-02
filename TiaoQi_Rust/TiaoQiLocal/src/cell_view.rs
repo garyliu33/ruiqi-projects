c340b8f8f8768e680f3ad5898331c99cb25dfc4b
Ruiqi Liu <ruiqiliu2014@gmail.com>
2025-09-02 00:01:31 -0700

    almost final gui

--- TiaoQi_Rust/TiaoQiLocal/src/cell_view.rs
+++ TiaoQi_Rust/TiaoQiLocal/src/cell_view.rs
@@ -1,12 +1,14 @@
-use macroquad::color::{BLACK, BLUE, GREEN, LIME, ORANGE, PURPLE, RED, WHITE, YELLOW};
+use std::f32::consts::PI;
+use macroquad::color::*;
 use macroquad::input::mouse_position;
+use macroquad::prelude::*;
 use macroquad::shapes::{draw_circle, draw_circle_lines};
 use crate::board::PieceColor;
 use crate::display_constants::RADIUS;
 
 pub struct CellView {
     index: usize,
-    pub(crate) color: Option<PieceColor>,
+    pub color: Option<PieceColor>,
     x: f32,
     y: f32,
     clickable: bool,
@@ -19,16 +21,33 @@ impl CellView {
     }
     
     pub fn draw(&self) {
-        if self.is_hovered() {
-            draw_circle_lines(self.x, self.y, RADIUS + 1.5, 3.0, LIME);
-        }
-        draw_circle_lines(self.x, self.y, RADIUS, 2.0, BLACK);
+        match self.color {
+            Some(piece_color) => {
+                let mut color = piece_color.get_display_color();
+                draw_circle(self.x, self.y, RADIUS, color);
+                draw_circle_lines(self.x, self.y, RADIUS, 2.0, WHITE);
 
-        if let Some(color) = &self.color {
-            if self.selected {
-                draw_circle(self.x, self.y, RADIUS, ORANGE)
-            } else {
-                draw_circle(self.x, self.y, RADIUS, color.get_display_color());
+                if self.selected {
+                    color = Color::new(1.0, 1.0, 1.0, 0.9);
+                    draw_mesh(&gradient_ring_mesh(self.x, self.y, RADIUS + 2.0, 15.0, color, transparent(color)));
+                } else if self.is_hovered() {
+                    color = Color::new(1.0, 1.0, 1.0, 0.6);
+                    draw_mesh(&gradient_ring_mesh(self.x, self.y, RADIUS + 2.0, 12.0, color, transparent(color)));
+                } else if self.clickable {
+                    color.a = 0.6;
+                    draw_mesh(&gradient_ring_mesh(self.x, self.y, RADIUS + 2.0, 12.0, color, transparent(color)));
+                }
+            }
+            None => {
+                draw_circle_lines(self.x, self.y, RADIUS, 2.0, WHITE);
+
+                let mut color = WHITE;
+                if self.is_hovered() {
+                    draw_mesh(&gradient_ring_mesh(self.x, self.y, RADIUS + 2.0, 12.0, color, transparent(color)));
+                } else if self.clickable {
+                    color.a = 0.4;
+                    draw_mesh(&gradient_ring_mesh(self.x, self.y, RADIUS + 2.0, 12.0, color, transparent(color)));
+                }
             }
         }
     }
@@ -57,4 +76,58 @@ impl CellView {
     pub fn index(&self) -> usize {
         self.index
     }
+}
+
+fn gradient_ring_mesh(x: f32, y: f32, radius: f32, thickness: f32, inner_color: Color, outer_color: Color) -> Mesh {
+    let mut vertices = vec![];
+    let mut indices = vec![];
+
+    let segments = 128;
+    for i in 0..=segments {
+        let angle = (i as f32 / segments as f32) * 2.0 * PI;
+        let cos = angle.cos();
+        let sin = angle.sin();
+
+        vertices.push(Vertex::new(
+            x + (radius + thickness) * cos,
+            y + (radius + thickness) * sin,
+            0.0,
+            0.0,
+            0.0,
+            outer_color,
+        ));
+
+        vertices.push(Vertex::new(
+            x + radius * cos,
+            y + radius * sin,
+            0.0,
+            0.0,
+            0.0,
+            inner_color,
+        ));
+    }
+
+    for i in 0..segments {
+        let current = i * 2;
+        let next = (i + 1) * 2;
+
+        let v0 = current;
+        let v1 = current + 1;
+        let v2 = next + 1;
+        let v3 = next;
+
+        indices.push(v0);
+        indices.push(v1);
+        indices.push(v2);
+
+        indices.push(v0);
+        indices.push(v2);
+        indices.push(v3);
+    }
+
+    Mesh{ vertices, indices, texture: None }
+}
+
+fn transparent(color: Color) -> Color {
+    Color::new(color.r, color.g, color.b, 0.0)
 }
--- TiaoQi_Rust/TiaoQiLocal/src/piece_color.rs
+++ TiaoQi_Rust/TiaoQiLocal/src/piece_color.rs
@@ -1,49 +1,37 @@
-use macroquad::color::{Color, BLUE, GRAY, GREEN, PURPLE, RED, YELLOW};
-use crate::piece_color::PieceColor::{Red, Yellow, Green, Blue, Purple, Gray};
+use macroquad::color::*;
+use crate::piece_color::PieceColor::{Red, Orange, Green, Blue, Purple, Cyan};
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum PieceColor {
     Red,
-    Yellow,
+    Orange,
     Green,
+    Cyan,
     Blue,
-    Purple,
-    Gray
+    Purple
 }
 
 impl PieceColor {
     pub fn get_color(i: usize) -> PieceColor {
         match i {
             0 => Red,
-            1 => Yellow,
+            1 => Orange,
             2 => Green,
-            3 => Blue,
-            4 => Purple,
-            5 => Gray,
+            3 => Cyan,
+            4 => Blue,
+            5 => Purple,
             _ => unreachable!()
-        }
-    }
-
-    pub fn get_index(&self) -> usize {
-        match self {
-            Red => 0,
-            Yellow => 1,
-            Green => 2,
-            Blue => 3,
-            Purple => 4,
-            Gray => 5
         }
     }
 
     pub fn get_display_color(&self) -> Color {
-        match self.get_index() {
-            0 => RED,
-            1 => YELLOW,
-            2 => GREEN,
-            3 => BLUE,
-            4 => PURPLE,
-            5 => GRAY,
-            _ => unreachable!()
+        match self {
+            Red => RED,
+            Orange => ORANGE,
+            Green => GREEN,
+            Cyan => Color::from_rgba(13, 212, 252, 255),
+            Blue => Color::from_rgba(0, 55, 255, 255),
+            Purple => PURPLE,
         }
     }
 }
