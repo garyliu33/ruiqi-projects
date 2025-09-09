6b1a7617c15641656d4891cf521eb2df528bf69f
a <garyliu33@gmail.com>
2025-09-08 22:01:38 -0700

    add server code

---
+++ SchottenTotten2_Java/online/clientserver/server/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id("com.google.protobuf") version "0.9.5"
+}
+
+group = "com.st"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation("com.google.protobuf:protobuf-java:4.32.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    protobuf(files("../../../../SchottenTotten2_proto/protos/"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
